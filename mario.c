b3a95260257a051138c0071878aff320d34c0045
Arunshaw-git <Arunshaw-git@users.noreply.github.com>
2023-08-09 21:11:47 +0530

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -1,35 +1,39 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main()
-{       int n;
-    do{
-    printf("height:\n");
-    scanf("%d",&n);
+{
+    int n;
+    do
+    {
+        printf("height:\n");
+        scanf("%d", &n);
     }
-    while(n>8||n<=0);
+    while (n > 8 || n <= 0);
 
-    for(int i=1; i<=n; i++){
+    for (int i = 1; i <= n; i++)
+    {
 
-      for(int j=1; j<=(n+i+1); j++){
+        for (int j = 1; j <= (n + i + 1); j++)
+        {
 
-      if((i+j)<=n){
+            if ((i + j) <= n)
+            {
 
-        printf(" ");
+                printf(" ");
+            }
+            else if (j == n + 1)
+            {
+                printf("  ");
+            }
+            else
+            {
 
-           }
-            else if (j == n+1){
-            printf("  ");
-           }
-        else{
-
-        printf("#");
-
-        }
-
+                printf("#");
+            }
         }
         printf("\n");
-        }
+    }
 
     return 0;
 }
