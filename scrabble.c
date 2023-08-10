0e0520f31b081b12628364d4c093f6e320713c01
Arunshaw-git <Arunshaw-git@users.noreply.github.com>
2023-08-10 20:46:58 +0530

    automated commit by submit50

--- scrabble.c
+++ scrabble.c
@@ -1,9 +1,9 @@
-#include<ctype.h>
-#include<cs50.h>
+#include <cs50.h>
+#include <ctype.h>
 
-#include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -17,25 +17,24 @@ int main(void)
     string word1 = get_string("Player 1: ");
     string word2 = get_string("Player 2: ");
 
-
     // Score both words
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    if(score1>score2){
+    if (score1 > score2)
+    {
 
-    printf("Player 1 wins!\n");
-
-
+        printf("Player 1 wins!\n");
     }
-    else if(score2>score1){
+    else if (score2 > score1)
+    {
 
         printf("Player 2 wins!\n");
-
-        }
-        else{
+    }
+    else
+    {
         printf("Tie!");
-        }
+    }
 
     // TODO: Print the winner
 }
@@ -44,31 +43,34 @@ int compute_score(char *word)
 {
     // TODO: Compute and return score for string
 
-    int n=0;
-    int sum=0;
-    for(int i =0;word[i]!='\0'; i++) {
+    int n = 0;
+    int sum = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
 
-      if(isalpha(word[i]) && islower(word[i])){
+        if (isalpha(word[i]) && islower(word[i]))
+        {
 
-       word[i] = (char)toupper((unsigned char)word[i]);
-
-      }
-
-      if( !isalpha(word[i] )){
-
-        break;
-
-      }
-
-    for(int j=0; j<26; j++){
-
-        if(word[i]==j+65){
-
-          sum+=POINTS[j];
-
-          break;
+            word[i] = (char) toupper((unsigned char) word[i]);
         }
+
+        if (!isalpha(word[i]))
+        {
+
+            break;
         }
+
+        for (int j = 0; j < 26; j++)
+        {
+
+            if (word[i] == j + 65)
+            {
+
+                sum += POINTS[j];
+
+                break;
+            }
         }
-        return sum;
+    }
+    return sum;
 }
