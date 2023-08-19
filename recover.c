11a039832c66f2cbfa34706d0e951b38e1dc8e17
Arunshaw-git <Arunshaw-git@users.noreply.github.com>
2023-08-19 20:45:29 +0530

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,80 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cs50.h>
-#include <ctype.h>
 #include <stdint.h>
+#include <ctype.h>
 
 typedef uint8_t byte;
 
+bool isjpg(byte *block);
+
 int main(int argc, char *argv[])
 {
-    int jpeg_count =0;
-    byte block[512];
 
-    FILE *img;
-    bool jpg_found = false;
-
-    if(argc !=2){
-        printf("file not found");
+    // ensure proper usage
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: recover forensicfilename.raw\n");
         return 1;
     }
 
-    FILE *inpt = fopen(argv[1] , "r");
 
-    if(inpt== NULL)
+    // open input file
+    FILE *inptr = fopen(argv[1], "r");
+    if (inptr == NULL)
     {
-
-        printf("file not found");
-
+        fprintf(stderr, "Could not open %s.\n", argv[1]);
         return 2;
     }
 
+    // Declare output file buffer ?
+    FILE *JPG;
 
- while (fread(block, sizeof(block), 1, inpt) == 1)
+    // Declare temporary storage (buffer) for block.
+    byte block[512];
+
+    // bool to see if we've found any jpgs yet
+    bool foundjpg = false;
+    int readblock;
+    int jpgnum = 0;
+
+
+
+    while (fread(block, sizeof(block), 1, inptr) == 1)
     {
 
-     fread(block,sizeof(block), 1, inpt);
+        // If we found a header
+        if (isjpg(block))
+        {
+
+            // Filename will be three digits + .jpg and a null character
+            char filename[8];
+
+            // filename is defined by jpgnum which only iterates when new jpgheader is found.
+            sprintf(filename, "%03.3i.jpg", jpgnum);
+
+            // For debugging
+            printf("processing file: %s\n", filename);
+
+            if (foundjpg == true)
+            {
+                // Already jpg in buffer. Close/finish it.
+                fclose(JPG);
+            }
+            else
+            {
+                // First jpg! Set foundjpg to true.
+                foundjpg = true;
+            }
+
+            // Open new and write first block in it
+            JPG = fopen(filename, "w");
+            fwrite(block, sizeof(block), 1, JPG);
 
 
+            jpgnum++;
+
+        }
+        // If we didn't find a header
+        else
+        {
+            // Are we already working on a jpg?
+            if (foundjpg == true)
+            {
+                // This is not a header and there is already a jpg, so write this block to current jpg.
+                fwrite(block, sizeof(block), 1, JPG);
+
+            }
+            // We only land here if we're in the beginning and still haven't found a header.
+            // We check again. If there happen to be zero headers in the whole file, loop exits and file closes.
+
+        }
+
+    }
+
+
+
+    fclose(inptr);
+    fclose(JPG);
+
+
+
+
+}
+
+bool isjpg(byte *block)
+{
     if (block[0] == 0xff &&
         block[1] == 0xd8 &&
         block[2] == 0xff &&
         (block[3] & 0xf0) == 0xe0)
     {
-        char name[10] ;
-
-       sprintf(name, "%03i.jpg" ,jpeg_count);
-       printf("processing file: %s\n", name);
-
-       if(jpg_found ==true ){
-
-        fclose(img);
-
-       }
-      else{
-
-     jpg_found = true;
-
-       }
-
-        img = fopen(name, "w");
-
-        fwrite(block,sizeof(block), 1, img);
-
-        jpeg_count++;
-
+        return true;
     }
-    else{
-
-        if(jpg_found==true){
-
-            fwrite(block,sizeof(block),1,img);
-
-        }
+    else
+    {
+        return false;
     }
-
- }
-  fclose(inpt);
-    fclose(img);
-
 }
