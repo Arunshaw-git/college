009b0881125b5e3bee4e22384ba3cf453a74c800
Arunshaw-git <Arunshaw-git@users.noreply.github.com>
2023-09-04 19:49:40 +0530

    automated commit by check50 [check50=True]

--- bank.py
+++ bank.py
@@ -1,14 +1,14 @@
 
 greet =input("Greeting: ")
 h = "hello"
-if greet.lower() in h:
-      print("$0", end='')
+if greet.lower()[0:5] in h:
+      print("$0", end="")
 
 elif greet[0] in ["h", "H"]:
       if greet.lower() != h:
-                print("$20", end='')
+                print("$20", end="")
 
 else:
-      print("$100", end='')
+      print("$100", end="")
 
 print("")
