d59d2c82bb536de44561b183018c2ac8ad79f9fb
Arunshaw-git <Arunshaw-git@users.noreply.github.com>
2023-09-11 22:30:07 +0530

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -2,28 +2,29 @@
 
 from cs50 import get_float
 
+
 def main():
-    cents =-1
+    cents = -1
     while True:
         try:
             cents = get_float("cents owed: ")
-            if cents>0 :
+            if cents > 0:
                 break
         except ValueError:
             print("invalid input")
-    cents= round(cents*100)
+    cents = round(cents * 100)
 
-    quaters = round(cents //25)
-    cents = round(cents - quaters *25)
+    quaters = round(cents // 25)
+    cents = round(cents - quaters * 25)
 
-    dimes = cents //10
-    cents = round(cents - dimes *10)
+    dimes = cents // 10
+    cents = round(cents - dimes * 10)
 
-    nickels = cents //5
-    cents = round(cents - nickels *5)
+    nickels = cents // 5
+    cents = round(cents - nickels * 5)
 
-    pennies = cents /1
-    cents = cents - pennies*1
+    pennies = cents / 1
+    cents = cents - pennies * 1
 
     coins = round(quaters + dimes + nickels + pennies)
 
