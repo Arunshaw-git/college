3a6972d78282b965e05a78c9bb3d1aa228b47e6d
Arun shaw <128923649+Arunshaw-git@users.noreply.github.com>
2025-07-29 22:45:25 +0530

    Delete practice directory

--- practice/index.js
+++
@@ -1,9 +0,0 @@
-const myBox = document.getElementById("box");
-
-function changecolor(event){
-    event.target.style.backgroundImage = "url('https://i1.sndcdn.com/artworks-ASgQ6A59p6tfUyeT-7DIEpg-t240x240.jpg')";
-    event.target.textContent ="";
-  
-}
-
-myBox.addEventListener("click",changecolor);
